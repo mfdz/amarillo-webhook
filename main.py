6811a618f30e0fadbc0d52c4efa4bad1912bd00f
siena <siena@x270>
2022-04-17 00:23:04 +0200

    Add restart when called on amarillo-cd itself

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import uvicorn
+import sys
 from fastapi import FastAPI, Body, Request
+from datetime import datetime
+
+print(f"Starting Amarillo-CD at {datetime.now().isoformat()}")
 
 app = FastAPI()
 
 
+
 @app.post("/payload")
 async def root(payload: dict = Body(...)):
+    is_amarillo_cd = payload.get('repository').get('name') == "amarillo-cd"
+
+    if is_amarillo_cd:
+        print(f"Exiting Amarillo-CD at {datetime.now().isoformat()}")
+        sys.exit(0)
+
     is_main_branch = payload.get('ref') == 'refs/heads/main'
     if is_main_branch:
         print("Push to main")
