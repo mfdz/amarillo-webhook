43d32f7db1dd18375831f78c7e45ada14e60bc03
siena <siena@x270>
2022-04-17 00:06:31 +0200

    Change port to 8888

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ async def root(payload: dict = Body(...)):
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8888)
