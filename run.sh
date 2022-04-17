3f36eed96b19a59c671ff26e607e0c0051f04819
Frank Gerhardt <frank.gerhardt@mitfahrdezentrale.de>
2022-04-18 01:21:05 +0200

    Hide pip output

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ do
    git pull
 
    pip install --upgrade -r requirements.txt > pip.log
-   [ $? -eq 0 ] && pip list || cat pip.log
+   [ $? -eq 0 ] && echo "Pip updated." || cat pip.log
 
    echo "Starting CD"
    gunicorn main:app -w 1 -k uvicorn.workers.UvicornWorker --name=amarillo-cd --bind=0.0.0.0:8888
