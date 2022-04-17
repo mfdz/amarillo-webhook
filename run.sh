839cf1af45c66337c516e5df966cc1c2e8798ac7
siena <siena@x270>
2022-04-18 00:48:42 +0200

    Make pip more quiet

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ do
 
    git pull
 
-   pip install -r requirements.txt
+   pip -q install -r requirements.txt
 
    echo "Starting CD"
    gunicorn main:app -w 1 -k uvicorn.workers.UvicornWorker --name=amarillo-cd --bind=0.0.0.0:8888
