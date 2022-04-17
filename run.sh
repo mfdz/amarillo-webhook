d487e6983aad07a9c31ce3f6e2fa3a28c33f8690
siena <siena@x270>
2022-04-18 00:34:56 +0200

    Change order or pull, pip, start

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 while true
 do
-   echo "Starting CD"
-   gunicorn main:app -w 1 -k uvicorn.workers.UvicornWorker --name=amarillo-cd --bind=0.0.0.0:8888
-
-   echo "CD exited"
 
    git pull
 
    pip install -r requirements.txt
+
+   echo "Starting CD"
+   gunicorn main:app -w 1 -k uvicorn.workers.UvicornWorker --name=amarillo-cd --bind=0.0.0.0:8888
+   echo "CD exited"
+
 done
