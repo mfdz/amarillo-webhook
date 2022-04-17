25a1578910f552383980e6ba2784f0b630a64440
Frank Gerhardt <frank.gerhardt@mitfahrdezentrale.de>
2022-04-18 01:25:37 +0200

    Changed messages

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ do
    pip install --upgrade -r requirements.txt > pip.log
    [ $? -eq 0 ] && echo "Pip updated." || cat pip.log
 
-   echo "Starting CD"
+   echo "CD starting."
    gunicorn main:app -w 1 -k uvicorn.workers.UvicornWorker --name=amarillo-cd --bind=0.0.0.0:8888
-   echo "CD exited"
+   echo "CD terminated."
 
 done
