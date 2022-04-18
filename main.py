ebc95e6daa7ffef2e3ece8065d20c15ab9ddf9c3
Frank Gerhardt <frank.gerhardt@mitfahrdezentrale.de>
2022-04-18 02:48:48 +0200

    Add logger

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, Body
 from util import guvicorn_process, is_amarillo_cd
 import signal
+import logging
 
-print(f"CD started.")
+logger = logging.getLogger(__name__)
+
+logger.info(f"CD started.")
 
 app = FastAPI()
 
@@ -10,7 +13,7 @@ app = FastAPI()
 @app.post("/payload")
 async def payload(payload: dict = Body(...)):
     if await is_amarillo_cd(payload):
-        print(f"CD sending SIGTERM to itself.")
+        logger.info(f"CD sending SIGTERM to itself.")
 
         # https://docs.gunicorn.org/en/stable/signals.html
         guvicorn_process().send_signal(signal.SIGTERM)
@@ -19,13 +22,13 @@ async def payload(payload: dict = Body(...)):
 
     is_main_branch = payload.get('ref') == 'refs/heads/main'
     if is_main_branch:
-        print(f"CD sending SIGTERM to amarillo-dev.")
+        logger.info(f"CD sending SIGTERM to amarillo-dev.")
         guvicorn_process(process_name="amarillo-dev").send_signal(signal.SIGTERM)
         return
 
     is_release_branch = payload.get('ref') == 'refs/heads/release'
     if is_release_branch:
-        print(f"CD sending SIGTERM to amarillo-prod.")
+        logger.info(f"CD sending SIGTERM to amarillo-prod.")
         guvicorn_process(process_name="amarillo-prod").send_signal(signal.SIGTERM)
         return
 
