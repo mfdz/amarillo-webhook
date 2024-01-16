cc5a9e36fe307256fec5d005e1f1b9ee5e95c4f2
Francia Csaba <cf@gerhardt.io>
2024-01-16 15:34:48 +0100

    #13 mitanand webhook

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException
 from util import gunicorn_process, is_amarillo_cd
 import signal
 import logging
@@ -49,6 +49,21 @@ async def payload(payload: dict = Body(...)):
         return
 
     print(payload)
+
+@app.post("/mitanand")
+async def payload_mitanand(payload: dict = Body(...)):
+    with open('secret.txt', 'r') as secret_file:
+        secret = secret_file.read().strip()
+        if payload.get('secret') != secret:
+            raise HTTPException(status_code=403, detail="Incorrect deploy secret")
+
+    logger.warning(("CD trying to stpp the amarillo-mitanand container"))
+
+    container = docker_client.containers.get("amarillo-mitanand")
+    logger.warning((f"CD stopping container {container}"))
+
+    container.stop()
+    logger.warning((f"CD container stopped"))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
